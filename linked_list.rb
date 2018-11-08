479d3766209443e80722120dd9dd9aa6645e7111
Anibel America <ameriabel@gmail.com>
2018-11-08 11:54:48 -0800

    implements additional methods to the LinkedLists class

--- linked_list.rb
+++ linked_list.rb
@@ -8,38 +8,82 @@ class LinkedList
   end
 
   def add_first(value)
-
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+    else
+      new_node.next = @head
+      @head = new_node
+    end
+    return new_node.data
   end
 
   def get_first
-
+    if @head.nil?
+      return @head
+    else
+      return @head.data
+    end
   end
 
   def length
-    return 0
+    if @head.nil?
+      return 0
+    else
+      count = 1
+      current = @head
+      until current.next.nil?
+        current = current.next
+        count += 1
+      end
+      return count
+    end
   end
 
   def add_last(value)
-    newNode = Node.new(value)
+    new_node = Node.new(value)
     if @head.nil?
       # List is empty
-      @head = newNode
+      @head = new_node
     else
       # List is not empty
       current = @head
       until current.next.nil?
         current = current.next
       end
-      current.next = newNode
+      current.next = new_node
     end
   end
 
   def get_last
-
+    if @head.nil?
+      return nil
+    else
+      current = @head
+      until current.next.nil?
+        current = current.next
+      end
+      return current.data
+    end
   end
 
   def get_at_index(index)
-
+    if @head.nil?
+      return nil
+    else
+      count = 0
+      current = @head
+      while count < index
+        if current.next.nil?
+          # if there are less nodes that index
+          return nil
+        else
+          current = current.next
+          count += 1
+        end
+      end
+      return current.data
+    end
   end
 
 end
